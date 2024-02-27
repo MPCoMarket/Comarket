1c5f84da6f3bed04637f3c3be107a762afd0f942
turtleYJ <yooil605@gmail.com>
2024-02-27 20:31:43 +0900

    project init

---
+++ comarket/src/main/java/com/part2/comarket/ComarketApplication.java
@@ -0,0 +1,13 @@
+package com.part2.comarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComarketApplication.class, args);
+	}
+
+}
---
+++ comarket/src/test/java/com/part2/comarket/ComarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.part2.comarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
